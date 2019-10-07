3297d8e5f1bafa76b5b6f70bd2ebe4e3994690dd
Kokobe <KobeChang@dhcp-10-8-027-068.mobile.reshsg.uci.edu>
2019-10-07 10:20:52 -0700

    spotlght

--- Assets/Spotlight_Spin.cs
+++ Assets/Spotlight_Spin.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Spotlight_Spin : MonoBehaviour
 {
+    public Vector3 rotAngle1;
+    public Vector3 rotAngle2;
+    Quaternion currentRot;
+    Quaternion firstRot;
+    Quaternion secondRot;
+    bool goingForward = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentRot = transform.rotation;
+        firstRot = Quaternion.Euler(rotAngle1);
+        secondRot = Quaternion.Euler(rotAngle2);
+    }
+
+    IEnumerator Rotate()
+    {
+        var t = 0f;
+        Quaternion from = transform.rotation;
+        while (t <= 1)
+        {
+            transform.rotation = (goingForward)? Quaternion.Lerp(from, secondRot, t) : Quaternion.Lerp(from, firstRot, t);
+            t += Time.fixedDeltaTime;
+            yield return null;
+        }
+        goingForward = !goingForward;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Rotate());
+        }
+
+
     }
 }
